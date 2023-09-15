41ef8793602589282f422bceef0029b36c6a5c33
Tanisha Kanal <144723509+tanisha0605@users.noreply.github.com>
2023-09-15 23:48:18 +0530

    Update SnakeGame.java

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,5 +1,5 @@
 public class SnakeGame {
-    public static void main(String[] args) {  // this is main file
-        new GameFrame();
+    public static void main(String[] args) {  // this is main file 
+        new GameFrame(); //new game frame
     }
 }
